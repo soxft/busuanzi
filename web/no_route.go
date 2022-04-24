43827edd2169b60ad417ce8012e5e66cb9a520bd
xcsoft <email@xcsoft.top>
2022-04-24 19:08:33 +0800

    feta: add a simple web server

--- web/no_route.go
+++ web/no_route.go
@@ -1,10 +1,36 @@
 package web
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"strings"
+)
 
 func NoRouteHandler(c *gin.Context) {
-	c.JSON(404, gin.H{
-		"success": false,
-		"message": "Page not found",
-	})
+	path := "./build" + c.Request.URL.Path
+	if strings.HasSuffix(path, "/") {
+		path += "index.html"
+	}
+	resource, err := ioutil.ReadFile(path)
+	if err != nil {
+		fmt.Println(path)
+		c.JSON(404, gin.H{
+			"success": false,
+			"message": "Page not found",
+		})
+		return
+	}
+	// web
+	ext := path[strings.LastIndex(path, "."):]
+	if ext == ".html" {
+		c.Header("Content-Type", "text/html")
+	} else if ext == ".js" {
+		c.Header("Content-Type", "text/javascript")
+	} else if ext == ".css" {
+		c.Header("Content-Type", "text/css")
+	}
+	c.String(200, string(resource))
+	return
+
 }
