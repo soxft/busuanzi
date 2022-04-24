8e7ac6cce9910bbe93e9594f0572721be2c339d7
xcsoft <email@xcsoft.top>
2022-04-24 20:33:45 +0800

    release: 1.0

---
+++ web/index.go
@@ -0,0 +1,9 @@
+package web
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func Index(c *gin.Context) {
+	c.HTML(200, "index.html", gin.H{})
+}
--- web/no_route.go
+++ web/no_route.go
@@ -1,36 +1,12 @@
 package web
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"io/ioutil"
-	"strings"
 )
 
 func NoRouteHandler(c *gin.Context) {
-	path := "./dist" + c.Request.URL.Path
-	if strings.HasSuffix(path, "/") {
-		path += "index.html"
-	}
-	resource, err := ioutil.ReadFile(path)
-	if err != nil {
-		fmt.Println(path)
-		c.JSON(404, gin.H{
-			"success": false,
-			"message": "Page not found",
-		})
-		return
-	}
-	// web
-	ext := path[strings.LastIndex(path, "."):]
-	if ext == ".html" {
-		c.Header("Content-Type", "text/html")
-	} else if ext == ".js" {
-		c.Header("Content-Type", "text/javascript")
-	} else if ext == ".css" {
-		c.Header("Content-Type", "text/css")
-	}
-	c.String(200, string(resource))
-	return
-
+	c.JSON(404, gin.H{
+		"success": false,
+		"message": "Page not found",
+	})
 }
