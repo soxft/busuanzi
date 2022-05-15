1872e921c256605cbac375b077e117bdca6495b8
soxft <email@xcsoft.top>
2022-05-16 02:10:06 +0800

    feta: 监听pushState

--- dist/busuanzi.js
+++ dist/busuanzi.js
@@ -1,22 +1,33 @@
 (function () {
     var url = "http://127.0.0.1:8080/?rand=" + Math.random();
     var tags = ["site_pv", "site_uv", "page_pv", "page_uv"];
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("x-bsz-referer", window.location.href);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                var res_1 = JSON.parse(xhr.responseText);
-                if (res_1.success === true) {
-                    tags.map(function (tag) {
-                        var ele = document.getElementById("busuanzi_".concat(tag));
-                        if (ele)
-                            ele.innerHTML = res_1['data'][tag];
-                    });
+    var bsz_send = function () {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader("x-bsz-referer", window.location.href);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    var res_1 = JSON.parse(xhr.responseText);
+                    if (res_1.success === true) {
+                        tags.map(function (tag) {
+                            var ele = document.getElementById("busuanzi_".concat(tag));
+                            if (ele)
+                                ele.innerHTML = res_1['data'][tag];
+                        });
+                    }
                 }
             }
-        }
+        };
+        xhr.send();
     };
-    xhr.send();
+    var history_pushState = window.history.pushState;
+    window.history.pushState = function () {
+        history_pushState.apply(this, arguments);
+        bsz_send();
+    };
+    window.addEventListener("popstate", function (_e) {
+        bsz_send();
+    }, false);
+    bsz_send();
 })();
--- dist/busuanzi.ts
+++ dist/busuanzi.ts
@@ -2,22 +2,35 @@
     let url: string = "http://127.0.0.1:8080/?rand=" + Math.random();
     let tags: string[] = ["site_pv", "site_uv", "page_pv", "page_uv"];
 
-    let xhr: XMLHttpRequest = new XMLHttpRequest();
-    xhr.open("POST", url, true);
+    let bsz_send: Function = function () {
+        let xhr: XMLHttpRequest = new XMLHttpRequest();
+        xhr.open("POST", url, true);
 
-    xhr.setRequestHeader("x-bsz-referer", window.location.href);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                let res: any = JSON.parse(xhr.responseText);
-                if (res.success === true) {
-                    tags.map((tag: string) => {
-                        let ele = document.getElementById(`busuanzi_${tag}`);
-                        if (ele) ele.innerHTML = res['data'][tag];
-                    })
+        xhr.setRequestHeader("x-bsz-referer", window.location.href);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    let res: any = JSON.parse(xhr.responseText);
+                    if (res.success === true) {
+                        tags.map((tag: string) => {
+                            let ele = document.getElementById(`busuanzi_${tag}`);
+                            if (ele) ele.innerHTML = res['data'][tag];
+                        })
+                    }
                 }
             }
         }
-    }
-    xhr.send();
+        xhr.send();
+    };
+
+    let history_pushState: Function = window.history.pushState;
+    window.history.pushState = function () {
+        history_pushState.apply(this, arguments);
+        bsz_send();
+    };
+
+    window.addEventListener("popstate", function (_e: PopStateEvent) {
+        bsz_send();
+    }, false);
+    bsz_send();
 })()
