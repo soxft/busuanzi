ff2fc24438b12d752e8d892a20d6679c168b4d21
soxft <email@xcsoft.top>
2022-06-09 19:43:43 +0800

    fix: header

--- app/middleware/cors.go
+++ app/middleware/cors.go
@@ -8,10 +8,10 @@ import (
 func Cors() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Header("Access-Control-Allow-Origin", config.Web.Cors)
-		c.Header("Server", "busuanzi-by-xcsoft/2.4")
+		c.Header("Server", "busuanzi-by-xcsoft/2.6")
 		if c.Request.Method == "OPTIONS" {
 			c.Header("Access-Control-Allow-Methods", "GET,POST,HEAD,OPTIONS")
-			c.Header("Access-Control-Allow-Headers", "x-bsz-referer")
+			c.Header("Access-Control-Allow-Headers", "x-bsz-referer, Authorization")
 			c.Header("Access-Control-Max-Age", "86400")
 			c.AbortWithStatus(204)
 			return
