9a1ecb19e7431776cd9a65d7747d39bfb8d63aa0
soxft <email@xcsoft.top>
2022-07-14 12:16:14 +0800

    release: 2.7.4

--- app/middleware/cors.go
+++ app/middleware/cors.go
@@ -8,7 +8,7 @@ import (
 func Cors() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Header("Access-Control-Allow-Origin", config.Web.Cors)
-		c.Header("Server", "busuanzi-by-xcsoft/2.7.3")
+		c.Header("Server", "busuanzi-by-xcsoft/2.7.4")
 		if c.Request.Method == "OPTIONS" {
 			c.Header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS")
 			c.Header("Access-Control-Allow-Headers", "x-bsz-referer, Authorization")
