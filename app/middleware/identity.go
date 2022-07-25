f5ef95cd018b145b77858aab7ac552c5952e6c0b
soxft <email@xcsoft.top>
2022-07-25 17:34:50 +0800

    optizmes: 优化bearer token字符数

--- app/middleware/identity.go
+++ app/middleware/identity.go
@@ -17,18 +17,18 @@ func Identity() gin.HandlerFunc {
 
 		if tokenTmp == "" {
 			// generate jwt token
-			userIdentity = tool.Md5(c.ClientIP()) + tool.Md5(c.Request.UserAgent())
+			userIdentity = getUserIdentity(c)
 			setBszIdentity(c, userIdentity)
 		} else {
 			token := strings.Replace(tokenTmp, "Bearer ", "", -1)
-
+			// check if token is illegal
 			if userIdentity = jwtutil.Check(token); userIdentity == "" {
 				// fake data, regenerate jwt token
-				userIdentity = tool.Md5(c.ClientIP()) + tool.Md5(c.Request.UserAgent())
+				userIdentity = getUserIdentity(c)
 				setBszIdentity(c, userIdentity)
 			}
 		}
-		c.Set("user_identity", tool.Md5(userIdentity))
+		c.Set("user_identity", userIdentity)
 		c.Next()
 	}
 }
@@ -36,4 +36,8 @@ func Identity() gin.HandlerFunc {
 func setBszIdentity(c *gin.Context, userIdentity string) {
 	uid := jwtutil.Generate(userIdentity)
 	c.Writer.Header().Set("Set-Bsz-Identity", uid)
+}
+
+func getUserIdentity(c *gin.Context) string {
+	return tool.Md5(c.ClientIP() + c.Request.UserAgent())
 }
