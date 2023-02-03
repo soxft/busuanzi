63ec8ae665baca06036d398857a50d62a19e7f45
soxft <email@xcsoft.top>
2023-02-03 23:34:33 +0800

    fix: xxx

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -x
 
 cd /app || exit
 
-if [ -f "install.lock" ];then
+if [ ! -f "/app/expose/install.lock" ];then
   # busuanzi js API address
   if [ -n "$API_SERVER" ];then
     sed -i "s/http:\/\/127.0.0.1:8080\/api/$API_SERVER/g" dist/busuanzi.js
@@ -12,8 +12,7 @@ if [ -f "install.lock" ];then
 
   mv dist /app/expose/dist
   mv config.yaml /app/expose/config.yaml
+  touch /app/expose/install.lock
 fi
-
-touch install.lock
 
 exec /app/busuanzi -c ./expose/config.yaml -d ./expose/dist
