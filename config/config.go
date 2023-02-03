cd934826f2741606064dcd52d80d2d4181244b45
soxft <email@xcsoft.top>
2023-02-03 21:56:59 +0800

    Feta: support read config in env

--- config/config.go
+++ config/config.go
@@ -5,6 +5,7 @@ import (
 	"gopkg.in/yaml.v2"
 	"log"
 	"os"
+	"strconv"
 )
 
 var (
@@ -19,20 +20,70 @@ var (
 	DistPath   string
 )
 
+var defaultConfig = Config{
+	Redis: RedisConfig{
+		Address:   "redis:6379",
+		Password:  "",
+		Database:  0,
+		Prefix:    "bsz_",
+		MaxIdle:   10,
+		MaxActive: 100,
+	},
+	Web: WebConfig{
+		Address: "0.0.0.0:8080",
+		Cors:    "*",
+		Log:     true,
+		Debug:   false,
+	},
+	Bsz: BszConfig{
+		Expire:    0,
+		JwtSecret: "bsz",
+	},
+}
+
 func init() {
 	// get config file path
 	flag.StringVar(&configPath, "c", "config.yaml", "config path")
 	flag.StringVar(&DistPath, "d", "dist", "dist path")
 	flag.Parse()
 
-	data, err := os.ReadFile(configPath)
-	if err != nil {
-		log.Fatal("Error reading config file:\r\n" + err.Error())
-	}
+	var data []byte
+	var err error
 	C = &Config{}
-	err = yaml.Unmarshal(data, C)
-	if err != nil {
-		log.Fatal("Error parsing config file:\r\n" + err.Error())
+
+	if data, err = os.ReadFile(configPath); err == nil {
+		if err = yaml.Unmarshal(data, C); err != nil {
+			log.Fatal("Error parsing config file:\r\n" + err.Error())
+		}
+	} else {
+		log.Println("Error reading config file:\r\n"+err.Error(), "Using default config", defaultConfig)
+		C = &defaultConfig
+	}
+
+	// READ FROM ENV
+	if _redisAddr, ok := os.LookupEnv("REDIS_ADDR"); ok {
+		C.Redis.Address = _redisAddr
+	}
+	if _redisPwd, ok := os.LookupEnv("REDIS_PWD"); ok {
+		C.Redis.Password = _redisPwd
+	}
+	if _redisDb, ok := os.LookupEnv("REDIS_DATABASE"); ok {
+		if _redisDbInt, err := strconv.Atoi(_redisDb); err == nil {
+			C.Redis.Database = _redisDbInt
+		}
+	}
+	if _log, ok := os.LookupEnv("LOG_ENABLE"); ok {
+		if _logBool, err := strconv.ParseBool(_log); err == nil {
+			C.Web.Log = _logBool
+		}
+	}
+	if _debug, ok := os.LookupEnv("DEBUG_ENABLE"); ok {
+		if _debugBool, err := strconv.ParseBool(_debug); err == nil {
+			C.Web.Debug = _debugBool
+		}
+	}
+	if _jwt, ok := os.LookupEnv("JWT_SECRET"); ok {
+		C.Bsz.JwtSecret = _jwt
 	}
 
 	Redis = C.Redis
