1431b6950d741adc5ef9c0ae8395559befc0ceb1
xcsoft <42080379+soxft@users.noreply.github.com>
2024-11-01 11:00:20 +0800

    fix: entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,15 +4,13 @@ set -x
 
 cd /app || exit
 
-if [ ! -f "/app/install.lock" ];then
+if [ ! -f "install.lock" ];then
   # busuanzi js API address
   if [ -n "$API_SERVER" ];then
     sed -i "s/http:\/\/127.0.0.1:8080\/api/$API_SERVER/g" dist/busuanzi.js
   fi
 
-  mv dist /app/dist
-  mv config.yaml /app/config.yaml
-  touch /app/install.lock
+  touch install.lock
 fi
 
 exec /app/busuanzi -c /app/config.yaml -d /app/dist
