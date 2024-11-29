d8e3ceae935729c9118f9ed754034ed5c842288c
soxft <email@xcsoft.top>
2024-11-29 15:53:14 +0800

    :wheelchair: perf: add API_JSONP_SERVER in entrypoint to specisify sed

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,17 @@ cd /app || exit
 if [ ! -f "/app/expose/install.lock" ];then
   # busuanzi js API address
   if [ -n "$API_SERVER" ];then
-    sed -i "s|http://127.0.0.1:8080/api|$API_SERVER|g" dist/*.js
+    sed -i "s|http://127.0.0.1:8080/api|$API_SERVER|g" dist/busuanzi.*.js
 
     echo "Replace API_SERVER to $API_SERVER"
   fi
+
+  # busuanzi js API address
+    if [ -n "$API_JSONP_SERVER" ];then
+      sed -i "s|http://127.0.0.1:8080/jsonp|$API_JSONP_SERVER|g" dist/busuanzi.jsonp.js
+
+      echo "Replace API_JSONP_SERVER to $API_JSONP_SERVER"
+    fi
 
   mv dist /app/expose/dist
   mv config.yaml /app/expose/config.yaml
